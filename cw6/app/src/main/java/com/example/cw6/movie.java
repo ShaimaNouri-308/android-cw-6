470e53ced5eaf9c9256a511725f57bd5d78936d1
ShaimaNouri-308 <66574499+ShaimaNouri-308@users.noreply.github.com>
2020-07-01 17:49:52 +0300

    cw 6 done

---
+++ cw6/app/src/main/java/com/example/cw6/movie.java
@@ -0,0 +1,9 @@
+package com.example.cw6;
+
+public class movie {
+    private String title;
+    private String mainActor;
+    private Double movieRate;
+    private int pgRate;
+    private String genre;
+}
